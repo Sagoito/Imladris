a44d3909a074e0534d754d5bebc875b01a775491
Sagoito <damian.ksiazek77@gmai.com>
2021-04-07 21:31:32 +0200

    Added mrcnn network to directory and modified files to make it work properly

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
+from skimage import io
+
 from src.data_preprocesing import prepare_data
+from src.mrcnn import visualize
 from src.settings import Setting
 import os
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import src.coco as network
 
 if __name__ == "__main__":
     os.makedirs(Path('src/images_all'), exist_ok=True)
     setting = Setting('src')
     prepare_data(setting)
+    model, class_names = network.get_model()
+
+
+    # # Check how model works and display results
+    #
+    # image = io.imread('src/test2017/000000289594.jpg')
+    # plt.figure(figsize=(12, 10))
+    # io.imshow(image)
+    # plt.show()
+    #
+    # # Run detection
+    # results = model.detect([image], verbose=1)
+    #
+    # # Visualize results
+    # r = results[0]
+    # visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+    #                             class_names, r['scores'])
+
