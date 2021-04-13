53c7bb4b9975456917d27d9fa822548b8f1d318d
LukaszMazurek <lukaszmazurek.contact@gmail.com>
2021-04-14 00:11:12 +0200

    Show segmented image

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import sys
+import os
+
 from skimage import io
 
 from src.data_preprocesing import prepare_data
 from src.mrcnn import visualize
 from src.settings import Setting
-import os
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import src.coco as network
-import threading
 
 from flask import Flask
 from flask import render_template, request, flash, redirect, session, url_for
 from werkzeug.utils import secure_filename
+
+from multiprocessing import Process, Queue
 
 
 app = Flask(__name__)
@@ -36,7 +39,7 @@ def index():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(setting.user_image, filename))
@@ -46,50 +49,56 @@ def index():
     return render_template('index.html')
 
 
-def segment_image(filename, model):
-    image = io.imread(filename)
-    # # Run detection
-    results = model.detect([image], verbose=1)
-    #
-    # # Visualize results
-    r = results[0]
-    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                                class_names, r['scores'])
-
-
-
 @app.route('/show_image', methods=['GET', 'POST'])
 def show_image():
     filename = session['filename']
-    file_path = f'user_image/{filename}'
-    return render_template('show_image.html', name=file_path)
+
+    model_process = Process(target=run_model, args=(filename,
+                                                    setting,
+                                                    clipboard, ))
+    model_process.start()
+    model_process.join()
+
+    filename_segmented = f"user_image/{clipboard.get()}"
+    print(filename_segmented)
+    return render_template('show_image.html', name=filename_segmented)
+
+
+def run_model(filename, setting, clipboard):
+    file_path = os.path.join(setting.user_image, filename)
+    model_class_names_tuple = network.get_model()
+    model = model_class_names_tuple[0]
+    class_names = model_class_names_tuple[1]
+
+    image = io.imread(file_path)
+    results = model.detect([image], verbose=1)
+
+    r = results[0]
+    image = visualize.display_instances(image, r['rois'], r['masks'],
+                                        r['class_ids'], class_names,
+                                        r['scores'], ret_image=True,
+                                        show=False)
+
+    filename = filename.split('.')[0]
+    f_segmented = f"{filename}_segmented.jpg"
+    save_path = os.path.join(setting.user_image, f_segmented)
+    io.imsave(save_path, image)
+    clipboard.put(f_segmented)
 
 
 if __name__ == "__main__":
-    """
-    app.config['SECRET_KEY'] = 'this_should_be_secret'
-    app.run(host='127.0.0.1', port=5000, debug=True)
-    """
+    global setting
+    global clipboard
+
+    """use queue for communication between processes"""
+    clipboard = Queue()
+
     setting = Setting('src')
     os.makedirs(Path('src/images_all'), exist_ok=True)
     setting = Setting('src')
-    prepare_data(setting)
-    model, class_names = network.get_model()
-    
 
-    # # Check how model works and display results
-    #
-    
-    image = io.imread('src/test2017/000000289594.jpg')
-    plt.figure(figsize=(12, 10))
-    io.imshow(image)
-    plt.show()
-    #
-    # # Run detection
-    results = model.detect([image], verbose=1)
-    #
-    # # Visualize results
-    r = results[0]
-    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                                class_names, r['scores'])
-    
+    if len(sys.argv) > 1 and sys.argv[1] == "prep":
+        prepare_data(setting)
+
+    app.config['SECRET_KEY'] = 'this_should_be_secret'
+    app.run(host='127.0.0.1', port=5000, debug=True, threaded=True)
