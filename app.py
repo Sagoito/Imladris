4c23d749902beb88d26b6c6282c202cb13b1c2b9
Sagoito <damian.ksiazek77@gmai.com>
2021-05-24 22:18:02 +0200

    Refactoring process that loaded model. Adding showing output image

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import ctypes
 import sys
 import os
 import cv2
@@ -18,12 +19,22 @@ from flask import Flask
 from flask import render_template, request, flash, redirect, session, url_for
 from werkzeug.utils import secure_filename
 
-from multiprocessing import Process, Queue, Manager
-
+from multiprocessing import Process, Queue, Manager, Value
 
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+@app.before_first_request
+def before_first_request():
+
+    model_process = Process(target=run_model, args=(setting,
+                                                    clipboard,
+                                                    run_statement))
+    try:
+        model_process.start()
+        # model_process.join()
+    except Exception as err:
+        print(err)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -54,8 +65,8 @@ def index():
 
 @app.route('/show_inpainted_image')
 def show_inpainted_image():
-    return "Here should be inpainted image"
-
+    filename_out = clipboard.get()
+    return render_template('show_image.html', name=filename_out)
 
 def prepare_masked_image():
     filename = session['filename']
@@ -94,21 +105,18 @@ def prepare_masked_image():
 @app.route('/show_image', methods=['GET', 'POST'])
 def show_image():
 
-    filename = session['filename']
-    model_process = Process(target=run_model, args=(filename,
-                                                    setting,
-                                                    clipboard,))
-
     if request.method == 'POST':
         prepare_masked_image()
+        filename_out = "user_image/out_" + session['filename']
+        clipboard.put(filename_out)
         return redirect(url_for('show_inpainted_image'))
-    
-    try:
-        model_process.start()
-        model_process.join()
-    except Exception as err:
-        print(err)
 
+    clipboard.put(session['filename'])
+    run_statement.value = True
+
+    while True:
+        if not run_statement.value:
+            break
     filename_segmented = f"{clipboard.get()}"
     filename_segmented = "user_image/" + filename_segmented
 
@@ -130,59 +138,65 @@ def show_image():
     return render_template('show_image.html', name=filename_segmented)
 
 
-def run_model(filename, setting, clipboard):
-    file_path = os.path.join(setting.user_image, filename)
+def run_model(setting, clipboard, run_statement):
     model_class_names_tuple = network.get_model()
     model = model_class_names_tuple[0]
     class_names = model_class_names_tuple[1]
+    print("Weights loaded")
+    while True:
+        if run_statement.value:
+            filename = clipboard.get()
+            file_path = os.path.join(setting.user_image, filename)
 
-    image = io.imread(file_path)
-    results = model.detect([image], verbose=1)
+            image = io.imread(file_path)
+            results = model.detect([image], verbose=1)
 
-    r = results[0]
-    image = visualize.display_instances(image, r['rois'], r['masks'],
-                                        r['class_ids'], class_names,
-                                        r['scores'], ret_image=True,
-                                        show=False)
-    
-    x_px = 0
-    y_px = 0
-    i_category = 0
-    detected_objects = {}
-    for obj in r['class_ids']:
-        detected_objects[i_category] = {}
-        i_category +=1
-    for y in r['masks']:
-        x_px = 0
-        for x in y:
-            i_category = 0           
-            for if_detected in x:          
-                if if_detected == True:
-                    if y_px not in detected_objects[i_category]:
-                        detected_objects[i_category][y_px]=[]
-                    detected_objects[i_category][y_px].append(x_px)
-                i_category+=1    
-            x_px+=1
-        y_px+=1            
+            r = results[0]
+            image = visualize.display_instances(image, r['rois'], r['masks'],
+                                                r['class_ids'], class_names,
+                                                r['scores'], ret_image=True,
+                                                show=False)
 
-                                       
-    filename = filename.split('.')[0]
-    f_segmented = f"{filename}_segmented.jpg"
-    save_path = os.path.join(setting.user_image, f_segmented)
-    io.imsave(save_path, image)
-    clipboard.put(f_segmented)
-    clipboard.put(detected_objects)
+            x_px = 0
+            y_px = 0
+            i_category = 0
+            detected_objects = {}
+            for obj in r['class_ids']:
+                detected_objects[i_category] = {}
+                i_category +=1
+            for y in r['masks']:
+                x_px = 0
+                for x in y:
+                    i_category = 0
+                    for if_detected in x:
+                        if if_detected == True:
+                            if y_px not in detected_objects[i_category]:
+                                detected_objects[i_category][y_px]=[]
+                            detected_objects[i_category][y_px].append(x_px)
+                        i_category+=1
+                    x_px+=1
+                y_px+=1
+
+            filename = filename.split('.')[0]
+            f_segmented = f"{filename}_segmented.jpg"
+            save_path = os.path.join(setting.user_image, f_segmented)
+            io.imsave(save_path, image)
+            clipboard.put(f_segmented)
+            clipboard.put(detected_objects)
+            run_statement.value = False
 
 
 if __name__ == "__main__":
     global setting
     global clipboard
+    run_statement = Value("i", False)
+
     """use queue for communication between processes"""
     clipboard = Manager().Queue()
     setting = Setting('src')
     os.makedirs(Path('src/images_all'), exist_ok=True)
     setting = Setting('src')
-    
+
     if len(sys.argv) > 1 and sys.argv[1] == "prep":
         prepare_data(setting)
 
